f18ac2a973a59a6c819fd674939f5747589135bc
Alan Dague-Greene <alan@basicrecipe.com>
2017-07-13 06:24:48 -0600

    Add progress bar component html

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -8,3 +8,24 @@ u('.js-ButtonGroupDemo > .Button').on('click', function() {
   u('.js-ButtonGroupDemo > .Button').removeClass('is-active');
   u(this).addClass('is-active');
 });
+
+// Example code for radial loading indicator
+// Building off example of adding in-line css based on percentage used for bar loading indicators
+
+var firstHalf = u('.ProgressBar--radial.demo .first-half').nodes[0],
+    secondHalf = u('.ProgressBar--radial.demo .second-half').nodes[0],
+    circleDiv = u('.ProgressBar--radial.demo .circle'),
+    loadingProgress = 85, // loading progress in precentage
+    loadingDegrees = (loadingProgress*360)/100; // convert percentage loaded to degrees rotation
+
+console.log(firstHalf);
+// rotate only firstHalf upto 180 degrees, second half if > 180 degrees
+if(loadingDegrees <= 180) {
+ firstHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
+ secondHalf.style.transform = 'rotate(0deg)';
+ circleDiv.removeClass('whole').addClass('half');
+} else if(loadingDegrees > 180) {
+ firstHalf.style.transform = 'rotate(180deg)';
+ secondHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
+ circleDiv.addClass('whole').removeClass('half');
+}
