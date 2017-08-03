cc4f4f1f03d6027e18794f14816774ed6525e1c3
Sara Lohr <sara.lohr@teamsnap.com>
2017-08-03 10:15:14 -0500

    Add button group documentation

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log(`toolkit.js is being used at ${Date.now()}.`);
+
+import {u} from 'umbrellajs';
+
+u('.js-ButtonGroupDemo > .Button').on('click', function() {
+  u('.js-ButtonGroupDemo > .Button').removeClass('is-active');
+  u(this).addClass('is-active');
+});
