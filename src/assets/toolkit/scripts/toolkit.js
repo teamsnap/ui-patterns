c2a201c2c8eac6020aa8b3d5b01598d337428e65
Courtney Hoskins <sea_dot@quartzfilms.com>
2017-10-13 16:41:54 -0600

    DES-201 tree menu structure

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -12,20 +12,37 @@ u('.js-ButtonGroupDemo > .Button').on('click', function() {
 // Example code for radial loading indicator
 // Building off example of adding in-line css based on percentage used for bar loading indicators
 
-var firstHalf = u('.ProgressBar--radial.demo .first-half').nodes[0],
+
+
+// rotate only firstHalf upto 180 degrees, second half if > 180 degrees
+var radialProgress = function() {
+  var firstHalf = u('.ProgressBar--radial.demo .first-half').nodes[0],
     secondHalf = u('.ProgressBar--radial.demo .second-half').nodes[0],
     circleDiv = u('.ProgressBar--radial.demo .circle'),
     loadingProgress = 85, // loading progress in precentage
+
     loadingDegrees = (loadingProgress*360)/100; // convert percentage loaded to degrees rotation
 
-console.log(firstHalf);
-// rotate only firstHalf upto 180 degrees, second half if > 180 degrees
-if(loadingDegrees <= 180) {
- firstHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
- secondHalf.style.transform = 'rotate(0deg)';
- circleDiv.removeClass('whole').addClass('half');
-} else if(loadingDegrees > 180) {
- firstHalf.style.transform = 'rotate(180deg)';
- secondHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
- circleDiv.addClass('whole').removeClass('half');
+  if(firstHalf && loadingDegrees <= 180) {
+    firstHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
+    secondHalf.style.transform = 'rotate(0deg)';
+    circleDiv.removeClass('whole').addClass('half');
+  } else if(firstHalf && loadingDegrees > 180) {
+    firstHalf.style.transform = 'rotate(180deg)';
+    secondHalf.style.transform = 'rotate(' + loadingDegrees + 'deg)';
+    circleDiv.addClass('whole').removeClass('half');
+  } else {
+    return;
+  }
 }
+
+radialProgress();
+
+u('.TreeMenu-branch').on('click', function(e) {
+  e.stopPropagation();
+  if(u(this).hasClass('collapsed')) {
+    u(this).removeClass('collapsed').addClass('open');
+  } else if(u(this).hasClass('open')) {
+    u(this).removeClass('open').addClass('collapsed');
+  }
+});
